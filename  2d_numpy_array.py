f27027918d1673ff2c3532a39d63827e6cf90930
Bonifase <orwabonifase@gmail.com>
2018-12-25 13:27:25 +0300

    2d numpy statistics

---  2d_numpy_array.py
+++  2d_numpy_array.py
@@ -41,3 +41,22 @@ conversion = np.array([0.0254, 0.453592])
 
 # Print out product of np_baseball and conversion
 print("2D Multiply Arithmetic: ", np_baseball * conversion)
+
+# Explore the baseball data
+# Because the mean and median are so far apart, you decide to complain to the MLB. They find the error and send the corrected data over to you. It's again available as a 2D Numpy array np_baseball, with three columns.
+
+# The Python script on the right already includes code to print out informative messages with the different summary statistics.
+avg = np.mean(np_baseball[:, 0])
+print("Average: " + str(avg))
+
+# Print median height. Replace 'None'
+med = np.median(np_baseball[:, 0])
+print("Median: " + str(med))
+
+# Print out the standard deviation on height. Replace 'None'
+stddev = np.std(np_baseball[:, 0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column. Replace 'None'
+corr = np.corrcoef(np_baseball[:, 0], np_baseball[:, 1])
+print("Correlation: " + str(corr))
