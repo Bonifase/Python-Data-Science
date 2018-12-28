e19f9bd7c5bf20c4f5f4f233a6b328633a0a8bbb
Bonifase <orwabonifase@gmail.com>
2018-12-28 12:22:10 +0300

    convert csv to dataframe

--- pandas_basics.py
+++ pandas_basics.py
@@ -35,3 +35,19 @@ cars.index = row_labels
 
 # Print cars again
 print(cars)
+
+
+# CSV to DataFrame (1)
+# Putting data in a dictionary and then building a DataFrame works,
+# but it's not very efficient.
+# What if you're dealing with millions of observations?
+# In those cases, the data is typically available as files
+# with a regular structure. One of those file types is the CSV file,
+# which is short for "comma-separated values".
+
+# To import CSV data into Python as a Pandas DataFrame you can use read_csv()
+# Import the cars.csv data: cars
+cars = pd.read_csv('cars.csv')
+
+# Print out cars
+print("cars", cars)
